25b06e34b866eb3b51aee550262cd104208e0a2d
Nick Peachey <nick.peachey@gmail.com>
2013-03-09 12:14:14 +0000

    set default page

--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,7 @@ RBugzilla::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'sprints#index'
 
   # See how all your routes lay out with "rake routes"
 
