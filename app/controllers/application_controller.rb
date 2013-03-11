378ad97864e395341ed667d0b04b3eb5190902dc
Nick Peachey <nickpeachey@Nicks-iMac.local>
2013-03-11 21:29:07 +0000

    added application crashing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from Exception, with: lambda { |exception| render_error 500, exception }
+    rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception| render_error 404, exception }
+  end
+
+  private
+  def render_error(status, exception)
+    respond_to do |format|
+      format.html { render template: "error/error404", layout: 'layouts/error_layout', status: status }
+      format.all { render nothing: true, status: status }
+    end
+  end
 end
